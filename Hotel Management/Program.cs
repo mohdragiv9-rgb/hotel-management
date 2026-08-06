33fd9b8985a6d484ed07e443b8528b80bd9594ed
Ragiv <mohdragiv9@gmail.com>
2026-08-06 13:51:57 +0530

    Added program.cs

--- Hotel Management/Program.cs
+++ Hotel Management/Program.cs
@@ -5,7 +5,8 @@ using HotelManagement.Repository;
 var builder = WebApplication.CreateBuilder(args);
 
 
-
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
